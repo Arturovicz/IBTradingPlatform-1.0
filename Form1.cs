f65a5ff88c1be0bfe5f08b01c7572f14220154e1
Valerij <thiccnessy@gmail.com>
2023-12-02 22:14:28 +0100

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using IBApi;
+
+
+namespace IBTradingPlatform
+{
+    public partial class Form1 : Form
+    {
+
+        delegate void SetTextCallback(string text);
+
+
+
+        IBTradingPlatform.EWrapperImpl ibClient;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            ibClient = new IBTradingPlatform.EWrapperImpl();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
