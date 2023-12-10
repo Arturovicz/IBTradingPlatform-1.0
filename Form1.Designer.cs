d9f14f279f99e4c2258b0bdb3ec81310538bf46e
Valerij <thiccnessy@gmail.com>
2023-12-10 20:50:29 +0100

    added disconnect, buy sell and timer. must debug numQuantity

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.btnConnect = new System.Windows.Forms.Button();
             this.cbSymbol = new System.Windows.Forms.ComboBox();
             this.lbData = new System.Windows.Forms.ListBox();
@@ -52,13 +53,20 @@
             this.tbBid = new System.Windows.Forms.TextBox();
             this.tbAsk = new System.Windows.Forms.TextBox();
             this.tbLast = new System.Windows.Forms.TextBox();
+            this.tbValidID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.btnSell = new System.Windows.Forms.Button();
+            this.btnBuy = new System.Windows.Forms.Button();
+            this.chkOutside = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.numQuantity)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numPrice)).BeginInit();
             this.SuspendLayout();
             // 
             // btnConnect
             // 
-            this.btnConnect.Location = new System.Drawing.Point(12, 12);
+            this.btnConnect.Location = new System.Drawing.Point(12, 24);
             this.btnConnect.Name = "btnConnect";
             this.btnConnect.Size = new System.Drawing.Size(121, 25);
             this.btnConnect.TabIndex = 0;
@@ -73,7 +81,7 @@
             "MSFT",
             "AAPL",
             "TSLA"});
-            this.cbSymbol.Location = new System.Drawing.Point(15, 78);
+            this.cbSymbol.Location = new System.Drawing.Point(15, 90);
             this.cbSymbol.Name = "cbSymbol";
             this.cbSymbol.Size = new System.Drawing.Size(121, 21);
             this.cbSymbol.TabIndex = 1;
@@ -85,7 +93,7 @@
             // lbData
             // 
             this.lbData.FormattingEnabled = true;
-            this.lbData.Location = new System.Drawing.Point(163, 178);
+            this.lbData.Location = new System.Drawing.Point(163, 190);
             this.lbData.Name = "lbData";
             this.lbData.Size = new System.Drawing.Size(418, 108);
             this.lbData.TabIndex = 2;
@@ -93,7 +101,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(312, 61);
+            this.label3.Location = new System.Drawing.Point(312, 73);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(55, 13);
             this.label3.TabIndex = 5;
@@ -102,7 +110,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(464, 62);
+            this.label4.Location = new System.Drawing.Point(464, 74);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(40, 13);
             this.label4.TabIndex = 6;
@@ -111,7 +119,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(59, 110);
+            this.label5.Location = new System.Drawing.Point(59, 122);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(31, 13);
             this.label5.TabIndex = 7;
@@ -120,7 +128,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(184, 110);
+            this.label6.Location = new System.Drawing.Point(184, 122);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(37, 13);
             this.label6.TabIndex = 8;
@@ -129,7 +137,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(312, 109);
+            this.label7.Location = new System.Drawing.Point(312, 121);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(56, 13);
             this.label7.TabIndex = 9;
@@ -138,7 +146,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(464, 110);
+            this.label8.Location = new System.Drawing.Point(464, 122);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(23, 13);
             this.label8.TabIndex = 10;
@@ -147,7 +155,7 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(15, 162);
+            this.label9.Location = new System.Drawing.Point(15, 174);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(22, 13);
             this.label9.TabIndex = 11;
@@ -156,7 +164,7 @@
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(12, 203);
+            this.label10.Location = new System.Drawing.Point(12, 215);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(25, 13);
             this.label10.TabIndex = 12;
@@ -165,7 +173,7 @@
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(9, 252);
+            this.label11.Location = new System.Drawing.Point(9, 264);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(27, 13);
             this.label11.TabIndex = 13;
@@ -174,7 +182,7 @@
             // Quantity
             // 
             this.Quantity.AutoSize = true;
-            this.Quantity.Location = new System.Drawing.Point(160, 62);
+            this.Quantity.Location = new System.Drawing.Point(160, 74);
             this.Quantity.Name = "Quantity";
             this.Quantity.Size = new System.Drawing.Size(79, 13);
             this.Quantity.TabIndex = 15;
@@ -183,7 +191,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(59, 62);
+            this.label1.Location = new System.Drawing.Point(59, 74);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(41, 13);
             this.label1.TabIndex = 16;
@@ -195,7 +203,7 @@
             this.cbTif.Items.AddRange(new object[] {
             "DAY",
             "GTC (Good-to-Cancel)"});
-            this.cbTif.Location = new System.Drawing.Point(460, 126);
+            this.cbTif.Location = new System.Drawing.Point(460, 138);
             this.cbTif.Name = "cbTif";
             this.cbTif.Size = new System.Drawing.Size(121, 21);
             this.cbTif.TabIndex = 17;
@@ -209,7 +217,7 @@
             "ARCA",
             "BATS",
             "ISLAND"});
-            this.cbMarket.Location = new System.Drawing.Point(460, 78);
+            this.cbMarket.Location = new System.Drawing.Point(460, 90);
             this.cbMarket.Name = "cbMarket";
             this.cbMarket.Size = new System.Drawing.Size(121, 21);
             this.cbMarket.TabIndex = 18;
@@ -217,7 +225,7 @@
             // 
             // numQuantity
             // 
-            this.numQuantity.Location = new System.Drawing.Point(163, 78);
+            this.numQuantity.Location = new System.Drawing.Point(163, 90);
             this.numQuantity.Maximum = new decimal(new int[] {
             10000,
             0,
@@ -240,7 +248,7 @@
             0,
             0,
             65536});
-            this.numPrice.Location = new System.Drawing.Point(315, 78);
+            this.numPrice.Location = new System.Drawing.Point(315, 90);
             this.numPrice.Maximum = new decimal(new int[] {
             10000,
             0,
@@ -258,7 +266,7 @@
             "MCT",
             "STP",
             "MOC"});
-            this.cbOrderType.Location = new System.Drawing.Point(12, 125);
+            this.cbOrderType.Location = new System.Drawing.Point(12, 137);
             this.cbOrderType.Name = "cbOrderType";
             this.cbOrderType.Size = new System.Drawing.Size(121, 21);
             this.cbOrderType.TabIndex = 21;
@@ -266,7 +274,7 @@
             // 
             // tbVisible
             // 
-            this.tbVisible.Location = new System.Drawing.Point(163, 126);
+            this.tbVisible.Location = new System.Drawing.Point(163, 138);
             this.tbVisible.Name = "tbVisible";
             this.tbVisible.Size = new System.Drawing.Size(100, 20);
             this.tbVisible.TabIndex = 22;
@@ -277,7 +285,7 @@
             this.cbPrimaryEx.FormattingEnabled = true;
             this.cbPrimaryEx.Items.AddRange(new object[] {
             "NASDAQ"});
-            this.cbPrimaryEx.Location = new System.Drawing.Point(315, 125);
+            this.cbPrimaryEx.Location = new System.Drawing.Point(315, 137);
             this.cbPrimaryEx.Name = "cbPrimaryEx";
             this.cbPrimaryEx.Size = new System.Drawing.Size(121, 21);
             this.cbPrimaryEx.TabIndex = 23;
@@ -285,33 +293,108 @@
             // 
             // tbBid
             // 
-            this.tbBid.Location = new System.Drawing.Point(12, 178);
+            this.tbBid.Location = new System.Drawing.Point(12, 190);
             this.tbBid.Name = "tbBid";
             this.tbBid.Size = new System.Drawing.Size(100, 20);
             this.tbBid.TabIndex = 24;
             this.tbBid.Text = "0";
+            this.tbBid.Click += new System.EventHandler(this.tbBid_Click);
             // 
             // tbAsk
             // 
-            this.tbAsk.Location = new System.Drawing.Point(12, 219);
+            this.tbAsk.Location = new System.Drawing.Point(12, 231);
             this.tbAsk.Name = "tbAsk";
             this.tbAsk.Size = new System.Drawing.Size(100, 20);
             this.tbAsk.TabIndex = 25;
             this.tbAsk.Text = "0";
+            this.tbAsk.Click += new System.EventHandler(this.tbAsk_Click);
             // 
             // tbLast
             // 
-            this.tbLast.Location = new System.Drawing.Point(12, 268);
+            this.tbLast.Location = new System.Drawing.Point(12, 280);
             this.tbLast.Name = "tbLast";
             this.tbLast.Size = new System.Drawing.Size(100, 20);
             this.tbLast.TabIndex = 26;
             this.tbLast.Text = "0";
+            this.tbLast.Click += new System.EventHandler(this.tbLast_Click);
+            // 
+            // tbValidID
+            // 
+            this.tbValidID.Location = new System.Drawing.Point(163, 28);
+            this.tbValidID.Name = "tbValidID";
+            this.tbValidID.Size = new System.Drawing.Size(100, 20);
+            this.tbValidID.TabIndex = 27;
+            this.tbValidID.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(165, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "Next Valid Order ID";
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(315, 24);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(120, 27);
+            this.btnDisconnect.TabIndex = 29;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            // 
+            // btnSell
+            // 
+            this.btnSell.BackColor = System.Drawing.Color.Red;
+            this.btnSell.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSell.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnSell.Location = new System.Drawing.Point(12, 307);
+            this.btnSell.Name = "btnSell";
+            this.btnSell.Size = new System.Drawing.Size(75, 23);
+            this.btnSell.TabIndex = 30;
+            this.btnSell.Text = "SELL";
+            this.btnSell.UseVisualStyleBackColor = false;
+            this.btnSell.Click += new System.EventHandler(this.btnSell_Click);
+            // 
+            // btnBuy
+            // 
+            this.btnBuy.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnBuy.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuy.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnBuy.Location = new System.Drawing.Point(12, 337);
+            this.btnBuy.Name = "btnBuy";
+            this.btnBuy.Size = new System.Drawing.Size(75, 23);
+            this.btnBuy.TabIndex = 31;
+            this.btnBuy.Text = "BUY";
+            this.btnBuy.UseVisualStyleBackColor = false;
+            this.btnBuy.Click += new System.EventHandler(this.btnBuy_Click);
+            // 
+            // chkOutside
+            // 
+            this.chkOutside.AutoSize = true;
+            this.chkOutside.Location = new System.Drawing.Point(460, 28);
+            this.chkOutside.Name = "chkOutside";
+            this.chkOutside.Size = new System.Drawing.Size(88, 17);
+            this.chkOutside.TabIndex = 32;
+            this.chkOutside.Text = "Outside RTH";
+            this.chkOutside.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chkOutside);
+            this.Controls.Add(this.btnBuy);
+            this.Controls.Add(this.btnSell);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbValidID);
             this.Controls.Add(this.tbLast);
             this.Controls.Add(this.tbAsk);
             this.Controls.Add(this.tbBid);
@@ -372,6 +455,13 @@
         private System.Windows.Forms.TextBox tbBid;
         private System.Windows.Forms.TextBox tbAsk;
         private System.Windows.Forms.TextBox tbLast;
+        private System.Windows.Forms.TextBox tbValidID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Button btnSell;
+        private System.Windows.Forms.Button btnBuy;
+        private System.Windows.Forms.CheckBox chkOutside;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,6 +16,9 @@ namespace IBTradingPlatform
     public partial class Form1 : Form
     {
 
+        int order_id = 0;
+        int timer1_counter = 5;
+
         delegate void SetTextCallback(string text);
         delegate void SetTextCallbackTickPrice(string _tickPrice);
 
@@ -155,6 +158,83 @@ namespace IBTradingPlatform
             }
         }
 
-        
+        public void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            ibClient.ClientSocket.eDisconnect();
+        }
+
+        private void tbAsk_Click(object sender, EventArgs e)
+        {
+            numPrice.Value = Convert.ToDecimal(tbAsk.Text);
+        }
+
+        private void tbBid_Click(object sender, EventArgs e)
+        {
+            numPrice.Value = Convert.ToDecimal(tbBid.Text);
+        }
+
+        private void tbLast_Click(object sender, EventArgs e)
+        {
+            numPrice.Value = Convert.ToDecimal(tbLast.Text);
+        }
+
+        private void btnSell_Click(object sender, EventArgs e)
+        {
+            string side = "Sell";
+            send_order(side);
+        }
+
+        private void btnBuy_Click(object sender, EventArgs e)
+        {
+            string side = "Buy";
+            send_order(side);
+        }
+
+
+        public void send_order(string side)
+        {
+            
+            IBApi.Contract contract = new IBApi.Contract();
+
+            
+            contract.Symbol = cbSymbol.Text;
+            contract.SecType = "STK";
+            contract.Exchange = cbMarket.Text;
+            contract.PrimaryExch = "ISLAND";
+            contract.Currency = "USD";
+
+            IBApi.Order order = new IBApi.Order();
+            
+            order.OrderId = order_id;
+            order.Action = side;
+            order.OrderType = cbOrderType.Text;
+            order.TotalQuantity = Convert.ToDouble(numQuantity.Value);
+            order.LmtPrice = Convert.ToDouble(numPrice.Value);
+
+            if (cbOrderType.Text == "STP")
+            {
+                order.AuxPrice = Convert.ToDouble(numPrice.Value);
+            }
+            
+            order.DisplaySize = Convert.ToInt32(tbVisible.Text);
+            order.OutsideRth = chkOutside.Checked;
+
+            ibClient.ClientSocket.placeOrder(order_id, contract, order);
+
+            order_id++;
+            tbValidID.Text = Convert.ToString(order_id);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (timer1_counter == 0)
+            {
+                timer1.Stop();
+                
+                numPrice.Value = Convert.ToDecimal(tbBid.Text);
+                timer1_counter = 5; 
+            }
+            timer1_counter--; 
+        }
     }
 }
